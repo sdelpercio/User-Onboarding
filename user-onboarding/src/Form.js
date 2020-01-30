92e778e5c709eee4c55b23f0d9b1a5c6d51059eb
Shawn DelPercio <sdelperc@gmail.com>
2020-01-30 16:34:34 -0500

    added formik hof; mapped props to values; validated inputs

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const Onboarding = () => {
+const Onboarding = ({ values, errors, touched }) => {
     return (
         <div>
             <Form>
                 <label>
                     Name:
                     <Field type='text' name='name' />
+                    { touched.name && errors.name && (
+                        <p className='errors'>{ errors.name }</p>
+                    ) }
                 </label>
                 <label>
                     Email:
                     <Field type='email' name='email' />
+                    { touched.email && errors.email && (
+                        <p className='errors'>{ errors.email }</p>
+                    ) }
                 </label>
                 <label>
                     Password:
                     <Field type='password' name='password' />
+                    { touched.password && errors.password && (
+                        <p className='errors'>{ errors.password }</p>
+                    ) }
                 </label>
                 <label>
                     <Field type='checkbox' name='tos' />
@@ -35,6 +45,20 @@ const FormikOnboarding = withFormik({
             password: '',
             tos: false
         }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup
+        .string()
+        .max(15, 'Name can be no more than 15 characters long')
+        .required(),
+        email: Yup.string().email().required(),
+        password: Yup
+        .string()
+        .min(6, 'Password must be at least 6 characters long')
+        .required(),
+    }),
+    handleSubmit(values, formikBag) {
+        console.log('submitting', values);
     }
 })(Onboarding);
 export default FormikOnboarding
